0f036eff60e8ee15d553963261c29876568e219c
KiraFreya <113983733+KiraFreya@users.noreply.github.com>
2022-09-20 20:03:36 +1000

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+    DDRD = 0b00001000; //set D3 as output
+	  PORTD = 0b00001000;  //set D3 to high
+	    
+	  while(1){
+	  	 PORTD |= 0b00001000;  //LED on
+	  	 _delay_ms(500);
+  
+	  	 PORTD = 0x00;  //LED off
+	  	 _delay_ms(500);
+	  }
+  
+	  return 0;
+}
